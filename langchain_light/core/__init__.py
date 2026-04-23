bc8768172583235bfa2f4466807f05a061a3ee86
chenshuai9101 <chenshuai9101@163.com>
2026-04-23 23:19:52 +0800

    add: langchain_light/core/__init__.py

---
+++ langchain_light/core/__init__.py
@@ -0,0 +1,6 @@
+"""LangChain Light - 轻量级AI Agent框架"""
+
+from .model_manager import ModelManager
+from .chain_engine import ChainEngine
+from .agent_runtime import AgentRuntime
+from .tool_registry import ToolRegistry
